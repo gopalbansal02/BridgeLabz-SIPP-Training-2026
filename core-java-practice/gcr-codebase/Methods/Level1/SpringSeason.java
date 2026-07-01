6eb9ecb40663fc319f98800c27d42f6add777027
Gopal Bansal <136996005+gopalbansal02@users.noreply.github.com>
2026-07-01 10:48:27 +0530

    Delete core-java-practice directory

--- core-java-practice/gcr-codebase/Methods/Level1/SpringSeason.java
+++
@@ -1,5 +0,0 @@
-package core-java-practice.gcr-codebase.programming-elements.Day5_methods.Level1;
-
-public class SpringSeason (4) {
-    
-}
--- core-java-practice/gcr-codebase/programming-elements/JavaProgrammingElementLevel2/perimeterOfSquare.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class perimeterOfSquare {
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int length= sc.nextInt();
-        int perimeter = length*4;
-        System.out.print("The perimeter of a square is " + perimeter);
-    }
-}
